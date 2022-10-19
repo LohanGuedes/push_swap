8b95567319f6a43e3b0a4c60bbfd1ed8d5b3fdc3
Lohan <lguedes@Gdk>
2022-10-19 16:42:19 -0300

    WORKING! - 7 points - need to add small 3 and 5 sort

--- main.c
+++ main.c
@@ -31,10 +31,10 @@ int	max_value(t_stack *stack)
 	return (max);
 }
 
-int	add_slack(t_stack *stack)
+unsigned	add_slack(t_stack *stack)
 {
 	int min;
-	int	slack;
+	unsigned	slack;
 	int i;
 
 	i = stack->size;
@@ -48,7 +48,7 @@ int	add_slack(t_stack *stack)
 	return (slack);
 }
 
-void	remove_slack(t_stack *stack, int slack)
+void	remove_slack(t_stack *stack, unsigned slack)
 {
 	int i;
 
@@ -76,20 +76,20 @@ void	push_all_a(t_stack *stack_a, t_stack *stack_b)
 		pa(stack_a, stack_b);
 }
 
-// int	valid(t_stack *stack, int current_bit)
-// {
-// 	int i;
+int	valid(t_stack *stack, int current_bit)
+{
+	int i;
 
 
-// 	i = stack->size;
-// 	while(--i)
-// 	{
-// 		if((unsigned int)(stack->list[i] >> current_bit) & 1 !=
-// 			(unsigned int)(stack->list[i-1] >> current_bit) & 1)
-// 			return (1);
-// 	}
-// 	return (0);
-// }
+	i = stack->size;
+	while(--i)
+	{
+		if((unsigned int)(stack->list[i] >> current_bit) & 1 !=
+			(unsigned int)(stack->list[i-1] >> current_bit) & 1)
+			return (1);
+	}
+	return (1);
+}
 
 void	sort_big_stack(t_stack *stack_a, t_stack *stack_b)
 {
@@ -97,25 +97,28 @@ void	sort_big_stack(t_stack *stack_a, t_stack *stack_b)
 	int	msb_pos;
 	int current_bit;
 	int n_operations;
-	int slack;
+	unsigned slack;
 
 	current_bit = 0;
-	msb_pos = find_msb_pos(max_value(stack_a)) + 1;
 	slack = add_slack(stack_a);
+	msb_pos = find_msb_pos(max_value(stack_a)) + 1;
 
 	while (current_bit < msb_pos)
 	{
-		n_operations = 0;
-		while (n_operations < stack_a->size)
+		if(valid(stack_a, current_bit))
 		{
-			temp = stack_a->list[stack_a->head];
-			if ((temp >> current_bit) & 1)
-				pb(stack_a, stack_b);
-			else
-				ra(stack_a);
-			n_operations++;
+			n_operations = 0;
+			while (n_operations < stack_a->size)
+			{
+				temp = stack_a->list[stack_a->head];
+				if ((temp >> current_bit) & 1)
+					ra(stack_a);
+				else
+					pb(stack_a, stack_b);
+				n_operations++;
+			}
+			push_all_a(stack_a, stack_b);
 		}
-		push_all_a(stack_a, stack_b);
 		current_bit++;
 	}
 	remove_slack(stack_a, slack);
@@ -128,11 +131,7 @@ int	main(int argc, char *argv[])
 
 	stack_a = s_build_and_populate(argc, argv);
 	stack_b = s_gen(argc - 1);
-
 	sort_big_stack(&stack_a, &stack_b);
-	// for(int i = 0; i < stack_a.size; i++)
-	//    	ft_printf("%i\n", stack_a.list[i]);
-
 }
 
 t_stack s_build_and_populate(int argc, char *argv[])
@@ -144,7 +143,7 @@ t_stack s_build_and_populate(int argc, char *argv[])
 
 	i = stack.size;
 	while(i--)
-		stack.list[i] = ft_atoi(argv[i - argc]);
+		stack.list[i] = ft_atoi(argv[argc - i - 1]);
 
 	stack.head = stack.size -1;
 	return (stack);
